bc219a56fa634be573364cc5b3cd77d576791084
Jisun Park <jisunpark.t@gmail.com>
2024-04-25 23:48:38 +0900

    format 변수 제거하기

--- practice/chapter01/statement.js
+++ practice/chapter01/statement.js
@@ -1,4 +1,12 @@
 export default function statement(invoice, plays) {
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      maximumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
+
   function playFor(aPerformance) {
     return plays[aPerformance.playId];
   }
@@ -47,20 +55,15 @@ export default function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    maximumFractionDigits: 2,
-  }).format;
 
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} ${perf.audience}석\n`;
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} ${perf.audience}석\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액 ${format(totalAmount / 100)}\n`;
+  result += `총액 ${usd(totalAmount)}\n`;
   result += `적립 포인트 ${volumeCredits}점\n`;
 
   return result;
