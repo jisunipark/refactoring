03d2c496c582fc2526f0a8185fb832ebc7f5366e
Jisun Park <jisunpark.t@gmail.com>
2024-05-09 02:17:19 +0900

    중간 데이터 생성을 전담하는 함수 분리

--- practice/chapter01/statement.js
+++ practice/chapter01/statement.js
@@ -1,4 +1,8 @@
 export default function statement(invoice, plays) {
+  createStatementData(invoice, plays);
+}
+
+function createStatementData(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
