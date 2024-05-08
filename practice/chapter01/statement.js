666c1e8a9603b268d1d66514be9a5d9250a044be
Jisun Park <jisunpark.t@gmail.com>
2024-05-09 02:28:11 +0900

    HTML 버전 작성

--- practice/chapter01/statement.js
+++ practice/chapter01/statement.js
@@ -24,3 +24,32 @@ function renderPlainText(data) {
     }).format(aNumber / 100);
   }
 }
+
+export function htmlStatement(invoice, plays) {
+  renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구내역 (고객명: ${data.customer})</h1>\n`;
+  result += '<table>\n';
+  result += '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n';
+
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>(${usd(perf.amount)} ${perf.audience}석)</td>\n`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+
+  result += '</table>\n';
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    maximumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
