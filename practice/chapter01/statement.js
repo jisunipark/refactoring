a8a859827cc9eff0b731810f5aa0e00bcc47dace
Jisun Park <jisunpark.t@gmail.com>
2024-05-09 02:13:29 +0900

    😓

--- practice/chapter01/statement.js
+++ practice/chapter01/statement.js
@@ -62,25 +62,25 @@ export default function statement(invoice, plays) {
   function totalVolumeCredits(data) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
+}
 
-  function renderPlainText(data) {
-    let result = `청구내역 (고객명: ${data.customer})\n`;
+function renderPlainText(data) {
+  let result = `청구내역 (고객명: ${data.customer})\n`;
 
-    for (let perf of data.performances) {
-      result += `${perf.play.name}: ${usd(perf.amount)} ${perf.audience}석\n`;
-    }
+  for (let perf of data.performances) {
+    result += `${perf.play.name}: ${usd(perf.amount)} ${perf.audience}석\n`;
+  }
 
-    result += `총액 ${usd(data.totalAmount)}\n`;
-    result += `적립 포인트 ${data.totalVolumeCredits}점\n`;
+  result += `총액 ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트 ${data.totalVolumeCredits}점\n`;
 
-    return result;
+  return result;
 
-    function usd(aNumber) {
-      return new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        maximumFractionDigits: 2,
-      }).format(aNumber / 100);
-    }
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      maximumFractionDigits: 2,
+    }).format(aNumber / 100);
   }
 }
