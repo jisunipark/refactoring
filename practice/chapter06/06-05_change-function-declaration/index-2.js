bb9415bbc44f1a976ed11aae51687c47c0356a1c
Jisun Park <jisunpark.t@gmail.com>
2024-05-23 11:25:39 +0900

    6-5 함수 선언 바꾸기 - 매개변수 추가하기 - after

--- practice/chapter06/06-05_change-function-declaration/index-2.js
+++ practice/chapter06/06-05_change-function-declaration/index-2.js
@@ -4,6 +4,12 @@ class Book {
   }
 
   addReservation(customer) {
+    this.zz_reservations.push(customer, false);
+  }
+
+  zz_addReservation(customer, isPriority) {
+    // 새로 추가한 매개변수를 사용하는지 확인
+    assert(isPriority === true || isPriority === false);
     this._reservations.push(customer);
   }
 }
